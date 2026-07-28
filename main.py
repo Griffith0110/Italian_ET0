94afc49b7dc07c9e8c35b68b7f2766e3849d4780
Griffith0110 <118409402+Griffith0110@users.noreply.github.com>
2026-07-28 18:48:48 +0200

    Add user input for geolocator user agent
    Allow user to input a custom user agent for geolocation.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import streamlit as st
 
 
 st.subheader("🌿 ET0", divider="green", text_alignment="center")
-
-geolocator = Nominatim(user_agent="ET0")
+user_agent=st.text_input("Insert an invented username")
+if user_agent:
+    geolocator = Nominatim(user_agent=user_agent)
 location_query = st.text_input(
     "Geoposition",
 )
